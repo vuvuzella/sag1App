4fc4fec02c632f3311b161427c570bb3bff0f92f
vuvuzella <j.tabacii@gmail.com>
2016-07-18 02:44:46 +0800

    added expressJS, server.js; modified package.json; For deployment to heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path    = require('path');
+
+var app = express();
+app.use(express.static(path.join(__dirname, '/')));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+  console.log('Express server now listening to port ' + 
+    app.get('port'));
+});
